cd510179aa4239510dc5995b0cdaaed5b061af82
janchel <janchel@gmail.com>
2025-12-22 12:33:05 +0800

    fixing kubernetes version and helm

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,10 +1,10 @@
 # IAM resources for AWS Load Balancer Controller
 data "aws_eks_cluster" "cluster" {
-  name = module.eks.cluster_id
+  name = module.eks.cluster_name
 }
 
 data "aws_eks_cluster_auth" "cluster" {
-  name = module.eks.cluster_id
+  name = module.eks.cluster_name
 }
 
 # Fetch the OIDC certificate thumbprint
